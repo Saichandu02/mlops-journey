183a448219422d1a89f0c523f6409a71350a1a5d
saichandu02 <saichandu.me@gmail.com>
2026-03-03 04:02:23 -0600

    added docker and api

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.11-slim
+WORKDIR /app
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+COPY . .
+EXPOSE 8000
+CMD ["uvicorn", "app:app", "--host", "192.168.0.116","--port","8000"]
---
+++ app.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+import numpy as np
+
+app = FastAPI()
+
+with open("model.pkl", "rb") as f:
+    model=pickle.load(f)
+class IrisInput(BaseModel):
+    sepal_length:float
+    sepal_width:float
+    petal_length:float
+    petal_width:float
+
+@app.post("/predict")
+
+def predict(input_data: IrisInput):
+    features = np.array([[
+        input_data.sepal_length,
+        input_data.sepal_width,
+        input_data.petal_length,
+        input_data.petal_width
+    ]])
+
+    prediction = model.predict(features)
+
+    species =["setosa","versicolor","virginica"]
+    result = species[prediction[0]]
+
+    return {"prediction": result}
+
+@app.get("/")
+
+def health_check():
+    return{"status":"API is runing"}
